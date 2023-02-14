f5e22472ea92d79d7b084bc8b15e693dbed780cf
terangel <angel.teran@elasthink.com>
2023-02-14 11:04:09 +0100

    Plugin methods added to Android.

--- android/src/main/java/com/elasthink/cap/clipboard/ClipboardPlugin.java
+++ android/src/main/java/com/elasthink/cap/clipboard/ClipboardPlugin.java
@@ -1,6 +1,5 @@
 package com.elasthink.cap.clipboard;
 
-import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
@@ -9,14 +8,29 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 @CapacitorPlugin(name = "Clipboard")
 public class ClipboardPlugin extends Plugin {
 
-    private Clipboard implementation = new Clipboard();
+    @PluginMethod
+    public void read(PluginCall call) {
+        call.unimplemented("Not yet implemented!");
+    }
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
-
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+    public void readURL(PluginCall call) {
+        call.unimplemented("Not yet implemented!");
     }
+
+    @PluginMethod
+    public void write(PluginCall call) {
+        call.unimplemented("Not yet implemented!");
+    }
+
+    @PluginMethod
+    public void writeURL(PluginCall call) {
+        call.unimplemented("Not yet implemented!");
+    }
+
+    @PluginMethod
+    public void clear(PluginCall call) {
+        call.unimplemented("Not yet implemented!");
+    }
+
 }
