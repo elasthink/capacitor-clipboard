5cac376552151d111307cb8f9865eff9c69e5e11
terangel <angel.teran@elasthink.com>
2023-02-14 10:30:34 +0100

    Simplifying the interface.

--- ios/Plugin/ClipboardPlugin.swift
+++ ios/Plugin/ClipboardPlugin.swift
@@ -1,60 +1,38 @@
 import Foundation
 import Capacitor
 
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitorjs.com/docs/plugins/ios
- */
 @objc(ClipboardPlugin)
 public class ClipboardPlugin: CAPPlugin {
     
-    /// Valor nulo.
     private let null = NSNull()
     
-    /// Lee del portapapeles un texto.
-    /// - Returns: Texto obtenido o nulo.
     @objc func read(_ call: CAPPluginCall) {
         call.unimplemented("Not yet implemented!")
     }
 
-    /// Lee del portapapeles una URL.
-    /// - Returns: URL obtenida o valor nulo.
     @objc func readURL(_ call: CAPPluginCall) {
-        // UIPasteboard.general.hasURLs ? UIPasteboard.general.url! : null
         let url = UIPasteboard.general.hasURLs ? UIPasteboard.general.url : nil;
         call.resolve([
             "url": url?.absoluteString ?? null
         ])
     }
 
-    /// Lee del portapapeles una imagen.
-    /// - Returns: Imagen o valor nulo.
     @objc func readImage(_ call: CAPPluginCall) {
         call.unimplemented("Not yet implemented!")
     }
     
-    /// Copia el texto especificado al portapapeles.
-    /// - Parameter call:
-    ///     - {string} text Texto especificado.
     @objc func write(_ call: CAPPluginCall) {
         call.unimplemented("Not yet implemented!")
     }
 
-    /// Copia la URL especificada al portapapeles.
-    /// - Parameter call:
-    ///     - {string} url URL especificada.
     @objc func writeURL(_ call: CAPPluginCall) {
         call.unimplemented("Not yet implemented!")
     }
 
-    /// Copia la imagen especificada al portapapeles.
-    /// - Parameter call:
-    ///     - {string} image Imagen especificada.
     @objc func writeImage(_ call: CAPPluginCall) {
         call.unimplemented("Not yet implemented!")
     }
     
-    /// Vacía el portapapeles.
     @objc func clear(_ call: CAPPluginCall) {
         UIPasteboard.general.items = []
         call.resolve()
--- src/definitions.ts
+++ src/definitions.ts
@@ -1,26 +1,53 @@
+/**
+ * Interfaz del plugin.
+ * @interface
+ */
 export interface ClipboardPlugin {
 
-  write(options: WriteOptions): Promise<void>;
-
-  writeURL(options: WriteOptions): Promise<void>;
-
-  writeImage(options: WriteOptions): Promise<void>;
-
+  /**
+   * Lee el contenido del portapapeles como texto.
+   * @return {text: string} Texto obtenido o nulo.
+   */
   read(): Promise<ReadResult>;
 
+  /**
+   * Lee del portapapeles una URL.
+   * @return {url: string} URL o valor nulo si el portapapeles está vacío o no tiene formato de URL.
+   */
   readURL(): Promise<ReadResult>;
 
-  readImage(): Promise<ReadResult>;
+  /**
+   * Copia al portapapeles el texto especificado.
+   * @param {string} text Texto especificado.
+   */
+  write(options: WriteOptions): Promise<void>;
 
+  /**
+   * Copia al portapapeles la URL especificada.
+   * @param {string} url URL especificada.
+   */
+  writeURL(options: WriteOptions): Promise<void>;
+
+  /**
+   * Limpia el contenido del portapapeles.
+   */
   clear(): Promise<void>;
 }
 
+/**
+ * Parámetros del método write/writeURL.
+ * @interface
+ */
 export interface WriteOptions {
-  value?: string;
-  label?: string;
+  text?: string;
+  url?: string;
 }
 
+/**
+ * Parámetros de respuesta del método read/readURL.
+ * @interface
+ */
 export interface ReadResult {
-  value: string;
-  type: string;
+  text?: string;
+  url?: string;
 }
